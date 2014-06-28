d4af478d2b67cdf57311c31fc5a101ee92f69b6f
Jonas Friedann <j@frd.mn>
2014-06-28 18:32:36 +0200

    /ping command

--- src/mn/frd/Ping/Ping.java
+++ src/mn/frd/Ping/Ping.java
@@ -1,14 +1,57 @@
 package mn.frd.Ping;
 
-import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
  
-public class Ping extends JavaPlugin {
+public class Ping extends JavaPlugin implements Listener {
+	@Override
 	public void onEnable(){
-		this.getLogger().info("Plugin has been enabled!");
+		// Register events
+		this.getServer().getPluginManager().registerEvents(this, this);
+		// Log loading
+		this.getLogger().info("Loaded " + this.getDescription().getName() + " v" + this.getDescription().getVersion());	
 	}
- 
+
+	@Override
 	public void onDisable(){
-		this.getLogger().info("Plugin has been disabled.");
+		// Log disabling
+		this.getLogger().info("Disabled " + this.getDescription().getName() + " v" + this.getDescription().getVersion());	
+	}
+	
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		// Define prefix
+		String prefix = "[Ping]";
+
+		//  /ping command
+		if (cmd.getName().equalsIgnoreCase("ping"))
+		{
+			// Check for arguments
+			if(args.length == 0) {
+				// Send command overview
+				sender.sendMessage(prefix + ChatColor.YELLOW + " Your ping:");
+				return true;
+			} else if(args.length == 1) {
+				// Send command overview
+				sender.sendMessage(prefix + ChatColor.YELLOW + " " + args[0].toString() + "'s ping:");
+				return true;
+			} else {
+				// Send command overview
+				sender.sendMessage(prefix + ChatColor.YELLOW + " Plugin help:");
+				return true;
+			}
+			
+		}	
+		return false;
+	}
+	
+	public int getPing(Player p) { 
+		CraftPlayer cp = (CraftPlayer) p; 
+		EntityPlayer ep = cp.getHandle(); 
+		return ep.ping; 
 	}
 }
