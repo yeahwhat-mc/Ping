b1bace42ec3493b2d589bb1a6620cd446c25b126
Jonas Friedann <j@frd.mn>
2014-06-28 19:29:11 +0200

    Add error checks

--- src/mn/frd/Ping/Ping.java
+++ src/mn/frd/Ping/Ping.java
@@ -25,12 +25,10 @@ public class Ping extends JavaPlugin implements Listener {
 	}
 	
 	
+	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		// Define prefix
 		String prefix = "[Ping]";
-
-		// Define player object
-		final Player p = (Player) sender;
 		
 		//  /ping command
 		if (cmd.getName().equalsIgnoreCase("ping"))
@@ -38,16 +36,29 @@ public class Ping extends JavaPlugin implements Listener {
 			// Check for arguments
 			if(args.length == 0) {
 				// Send command overview
-				sender.sendMessage(prefix + " Your ping: " + getPing(p) + " ms");
-				return true;
+				if (!(sender instanceof Player)){
+					sender.sendMessage(prefix + " " + "Your ping: 0 ms you silly console!");
+					return true;
+				} else {
+					// Define player object
+					final Player p = (Player) sender;
+					sender.sendMessage(prefix + " " + "Your ping: " + getPing(p) + " ms");
+					return true;
+				}
 			} else if(args.length == 1) {
 				Player argplayer = getServer().getPlayer(args[0]);
+				if(argplayer == null) {
+				    sender.sendMessage(prefix + " " + "The player could not be found");
+				    return true;
+				}
 				// Send command overview
-				sender.sendMessage(prefix + " " + args[0].toString() + "'s ping: "+ getPing(argplayer) + " ms");
+				sender.sendMessage(prefix + " " + getServer().getPlayer(args[0]).getDisplayName().toString() + "'s ping: "+ getPing(argplayer) + " ms");
 				return true;
 			} else {
 				// Send command overview
-				sender.sendMessage(prefix + " Plugin help:");
+				sender.sendMessage(prefix + " " + "Plugin help:");
+				sender.sendMessage(prefix + " " + "/ping - Check your ping");
+				sender.sendMessage(prefix + " " + "/ping <player> - Check ping of player");
 				return true;
 			}
 			
