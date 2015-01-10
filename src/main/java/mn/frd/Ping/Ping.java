1f8f5437a11447a70f3a125075686a540837cc61
Jonas Friedmann <j@frd.mn>
2015-01-10 20:21:44 +0100

    Adjust for 1.8

--- src/main/java/mn/frd/Ping/Ping.java
+++ src/main/java/mn/frd/Ping/Ping.java
@@ -1,37 +1,42 @@
 package mn.frd.Ping;
 
-import net.minecraft.server.v1_7_R3.EntityPlayer;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_7_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class Ping extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable(){
 		// Register events
 		this.getServer().getPluginManager().registerEvents(this, this);
 		// Log loading
-		this.getLogger().info("Loaded " + this.getDescription().getName() + " v" + this.getDescription().getVersion());	
+		this.getLogger().info("Loaded " + this.getDescription().getName() + " v" + this.getDescription().getVersion());
 	}
 
 	@Override
 	public void onDisable(){
 		// Log disabling
-		this.getLogger().info("Disabled " + this.getDescription().getName() + " v" + this.getDescription().getVersion());	
+		this.getLogger().info("Disabled " + this.getDescription().getName() + " v" + this.getDescription().getVersion());
 	}
-	
-	
+
+	Class<?> CPClass;
+
+	String serverName  = Bukkit.getServer().getClass().getPackage().getName(),
+				 serverVersion = serverName.substring(serverName.lastIndexOf(".") + 1, serverName.length());
+
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		// Define prefix
 		String prefix = ChatColor.RED + "[Ping]" + ChatColor.RESET;
 
-		
+
 		//  /ping command
 		if (cmd.getName().equalsIgnoreCase("ping"))
 		{
@@ -46,7 +51,7 @@ public class Ping extends JavaPlugin implements Listener {
 						// Define player object
 						final Player p = (Player) sender;
 						sender.sendMessage(prefix + " " + "Your ping: " + ChatColor.GRAY + getPing(p) + " ms" + ChatColor.RESET);
-						return true;						
+						return true;
 					} else {
 						sender.sendMessage(prefix + " " +  "You dont have the permission: " + ChatColor.GRAY + "- ping.self");
 						return true;
@@ -56,8 +61,8 @@ public class Ping extends JavaPlugin implements Listener {
 				if(sender.hasPermission("ping.others")){
 					Player argplayer = getServer().getPlayer(args[0]);
 					if(argplayer == null) {
-					    sender.sendMessage(prefix + " " + "The player " + ChatColor.GRAY + args[0] + ChatColor.RESET +" could not be found");
-					    return true;
+							sender.sendMessage(prefix + " " + "The player " + ChatColor.GRAY + args[0] + ChatColor.RESET +" could not be found");
+							return true;
 					}
 					// Send command overview
 					sender.sendMessage(prefix + " " + getServer().getPlayer(args[0]).getDisplayName().toString() + "'s ping: " + ChatColor.GRAY + getPing(argplayer) + " ms" + ChatColor.RESET);
@@ -73,14 +78,25 @@ public class Ping extends JavaPlugin implements Listener {
 				sender.sendMessage("/ping <player>" + ChatColor.GRAY + " - " + ChatColor.GOLD + "Check ping of player.");
 				return true;
 			}
-			
-		}	
+
+		}
 		return false;
 	}
-	
-	public int getPing(Player p) { 
-		CraftPlayer cp = (CraftPlayer) p; 
-		EntityPlayer ep = cp.getHandle(); 
-		return ep.ping; 
+
+	public int getPing(Player p) {
+		try {
+			CPClass = Class.forName("org.bukkit.craftbukkit." + serverVersion + ".entity.CraftPlayer");
+			Object CraftPlayer = CPClass.cast(p);
+
+			Method getHandle = CraftPlayer.getClass().getMethod("getHandle", new Class[0]);
+			Object EntityPlayer = getHandle.invoke(CraftPlayer, new Object[0]);
+
+			Field ping = EntityPlayer.getClass().getDeclaredField("ping");
+
+			return ping.getInt(EntityPlayer);
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+		return 0;
 	}
 }
